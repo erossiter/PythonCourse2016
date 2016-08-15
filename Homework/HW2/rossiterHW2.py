3cc3c9dffd5189e47293d6ed4b1118293c17a0ae
erossiter <erinrossiter@wustl.edu>
2016-08-15 16:54:42 -0500

    Starting over.
    found a better way to navigate and make sure I get all forms.  Found
    max # of pages in the source code.

--- Homework/HW2/rossiterHW2.py
+++ Homework/HW2/rossiterHW2.py
@@ -4,50 +4,48 @@ import urllib2
 import csv
 
 
+def gotoLink(link):
 
-def searchJudge(asG, html):
-	#fin_disclosures = all_html.find("div", {"id" : "fin_disclosures"})
-	#html_to_links = fin_disclosures.find_all("span", {"class" : "title"})
+
+def searchJudge(web_page):
+	all_html = BeautifulSoup(web_page.read())
+	fin_disclosures = all_html.find("div", {"id" : "fin_disclosures"})
+	html_to_links = fin_disclosures.find_all("span", {"class" : "title"})
 
 	all_links = []
 	for l in html_to_links:
 		all_links.append(l.find("a", href=True)["href"])
+	return all_links
+
 
 def checkJudge(asF):
-	first_name, middle_int, last_name, conf_year = judgeInfo(asF)
-	if conf_year < 2013:
-		return "%s %s. %s" % (first_name, middle_int, last_name)
-	else:
-		return None
-
-
-def judgeInfo(asF):
 	my_reader = csv.reader(asF)
   	my_reader.next()
   	x = my_reader.next()
   	last_name = x[1]
   	first_name = x[2]
-  	middle_name = x[3]
-  	confirmation = x[37]
-  	return first_name, middle_name[0], last_name, int(confirmation[6:])
+  	middle_int = x[3][0]
+  	confirmation_year = int(x[37][6:])
+
+	if confirmation_year < 2013:
+		return "%s+%s.+%s" % (first_name, middle_int, last_name)
+	else:
+		return None
 
 
 def main():
 	with open("federalJudges.csv", 'rb') as f:
 		for i in range(0, 10):
-			print checkJudge(asF=f)
-
-	with open('judicialWatch.csv', 'wb') as g:
-		w = csv.DictWriter(g, fieldnames=("Name", "Year", "Link"))
-		w.writeheader()
-
-		web_address = "http://www.judicialwatch.org/judicial-financial-disclosure/?q="
-		web_page = urllib2.urlopen(web_address)
-		all_html = BeautifulSoup(web_page.read())
-
-		searchJudge(g, all_html)
+			the_judge = checkJudge(asF=f)
+			if the_judge != None:
+				start_address = "http://www.judicialwatch.org/judicial-financial-disclosure/?q=" + the_judge
+				web_page = urllib2.urlopen(start_address)
+				searchJudge(web_page)
 
 
+	#with open('judicialWatch.csv', 'wb') as g:	
+	#	w = csv.DictWriter(g, fieldnames=("Name", "Year", "Link"))
+	#	w.writeheader()
 
 
 main()
