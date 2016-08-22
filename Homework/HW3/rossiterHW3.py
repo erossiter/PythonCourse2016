c87ac1bb0c6e25abb8d4cab87b848a59d78b6718
erossiter <erinrossiter@wustl.edu>
2016-08-21 19:26:39 -0500

    Done with HW3

--- Homework/HW3/rossiterHW3.py
+++ Homework/HW3/rossiterHW3.py
@@ -20,10 +20,11 @@ def search_one(user, command):
 				time.sleep(1)
 	return master_list
 
-## Function to generate a master list of followers/friends
-## from a list of user objects
+## Function to generate a master list of user objects including
+## those objects supplied in the 'user_list' parameter and all of
+## their followers/friends ('command' parameter)
 def two_degree_search(user_list, command):
-	master_list = []
+	master_list = user_list
 	for u in user_list:
 		not_finished = True
 		while not_finished:
@@ -43,7 +44,7 @@ def user_activity(user_list, command):
 	for u in user_list:
 		total.append(eval(command))
 	index = total.index(max(total))
-	return user_list[index].id, user_list[index].name
+	return str(user_list[index].screen_name)
 
 ## Function to categorize a list of user objects
 def categorize_users(user_list):
@@ -67,7 +68,7 @@ def answers():
 	cat_friends = twitt.api.friends(target_account.id, count = 100)
 
 	## One degree of separation ##
-	print "Among the followers of your target who is the most active?"
+	print "\nAmong the followers of your target who is the most active?"
 	print user_activity(cat_followers, "u.statuses_count")
 
 	print "\nAmong the followers of your target who is the most popular?"
@@ -75,9 +76,9 @@ def answers():
 
 	print "\nAmong the friends of your target, who are the most active layman, expert and celebrity?"
 	cat_friends_layman, cat_friends_expert, cat_friends_celebrity = categorize_users(cat_friends)
-	print user_activity(cat_friends_layman, "u.statuses_count")
-	print user_activity(cat_friends_expert, "u.statuses_count")
-	print user_activity(cat_friends_celebrity, "u.statuses_count")
+	print "Layman: %s" % user_activity(cat_friends_layman, "u.statuses_count")
+	print "Expert: %s" % user_activity(cat_friends_expert, "u.statuses_count")
+	print "Celebrity: %s" % user_activity(cat_friends_celebrity, "u.statuses_count")
 
 	print "\nAmong the friends of your target who is the most popular?"
 	print user_activity(cat_friends, "u.followers_count")
@@ -87,14 +88,24 @@ def answers():
 	cat_followers_layman, cat_followers_expert, cat_followers_celebrity = categorize_users(cat_followers)
 	master_list_followers = two_degree_search((cat_followers_layman + cat_followers_expert), "twitt.api.followers")
 	all_followers_layman, all_followers_expert, all_followers_celebrity = categorize_users(master_list_followers)
-	print user_activity((all_followers_layman + all_followers_expert)[1:4], "u.statuses_count")
+	print user_activity((all_followers_layman + all_followers_expert), "u.statuses_count")
 
 	print "\nAmong the friends of your target and their friends, who is the most active?"
 	master_list_friends = two_degree_search((cat_friends_layman + cat_friends_expert), "twitt.api.followers")
 	all_friends_layman, all_friends_expert, all_friends_celebrity = categorize_users(master_list_friends)
-	print user_activity((all_friends_layman + all_friends_expert)[1:4], "u.statuses_count")
+	print user_activity((all_friends_layman + all_friends_expert), "u.statuses_count")
 
-answers()
+
+#answers()
+
+for i in range(0, 100):
+	not_finished = True
+	while not_finished:
+		try:
+			answers()
+			not_finished = False
+		except tweepy.RateLimitError:
+			time.sleep(1)
 
 	
 
