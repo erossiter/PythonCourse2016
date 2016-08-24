919cba382610ecfd53881e9ec318a095f433962e
erossiter <erinrossiter@wustl.edu>
2016-08-24 17:22:52 -0500

    Done with basic simulation and plot.
    Will probably change how I’m timing the functions… I realize it’s not
    how you’re supposed to be doing it.

---
+++ Homework/HW4/hw4_simulation.py
@@ -0,0 +1,54 @@
+from hw4_functions import *
+import timeit
+import random
+import numpy
+
+## Function to run the two sort methods with
+## a random sample of size 'n' with that sample
+## drawn from 1:r range. (below I set that range
+## to 1000)
+## The function times how long it takes for each
+## function to execute on the same random sample.
+def sort_simulation(n, r):
+	random_list = random.sample(range(r), n)
+
+	start_time = timeit.default_timer()
+	insertion_sort(random_list)
+	is_time = timeit.default_timer() - start_time
+	
+	start_time = timeit.default_timer()
+	merge_sort(random_list)
+	ms_time = timeit.default_timer() - start_time
+
+	return is_time, ms_time
+
+## This loop calls 'sort_simulation()' with an
+## increasing n.
+is_times = []
+ms_times = []
+for n in range(1, 100):
+	is_time, ms_time = sort_simulation(n, 1000)
+	is_times.append(is_time)
+	ms_times.append(ms_time)
+
+
+## This loop calls 'sort_simulation()' 100 times
+## for each n in order to average the time it
+## takes for different random samples.
+is_avg = []
+ms_avg = []
+for n in range(1, 101):
+	is_avg_these = []
+	ms_avg_these = []
+	for i in range(100):
+		is_time, ms_time = sort_simulation(n, 1000)
+		is_avg_these.append(is_time)
+		ms_avg_these.append(ms_time)
+	is_avg.append(numpy.mean(is_avg_these))
+	ms_avg.append(numpy.mean(ms_avg_these))
+
+
+
+
+
+
